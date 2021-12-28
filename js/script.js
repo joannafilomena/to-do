c6675afe742577cfe199131238e332ae6095b641
Joanna Kamińska <joannakaminska-92@wp.pl>
2021-12-28 13:22:42 +0100

    Full js functionality

--- js/script.js
+++ js/script.js
@@ -10,20 +10,6 @@
       },
    ];
 
-   const render = () => {
-      let htmlString = "";
-
-      for (const task of tasks) {
-         htmlString += `
-        <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
-        ${task.content}
-        </li>
-        `;
-      }
-
-      document.querySelector(".js-tasks").innerHTML = htmlString;
-   };
-
    const addNewTask = (newTaskContent) => {
       tasks.push({
          content: newTaskContent,
@@ -31,6 +17,48 @@
 
       render();
    }
+
+   const removeTask = (taskIndex) => {
+      tasks.splice(taskIndex, 1);
+      render();
+   }
+
+   const toggleTaskDone = (taskIndex) => {
+      tasks[taskIndex].done = !tasks[taskIndex].done;
+      render();
+   }
+
+   const render = () => {
+      let htmlString = "";
+
+      for (const task of tasks) {
+         htmlString += `
+        <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+        <button class="js-done">Zrobione?</button>
+        <button class="js-remove">Usuń</button>
+        ${task.content}
+        </li>
+        `;
+      }
+
+      document.querySelector(".js-tasks").innerHTML = htmlString;
+
+      const removeButtons = document.querySelectorAll(".js-remove");
+
+      removeButtons.forEach((removeButton, index) => {
+         removeButton.addEventListener("click", () => {
+            removeTask(index);
+         });
+      });
+
+      const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+      toggleDoneButtons.forEach((toggleDoneButton, index) => {
+         toggleDoneButton.addEventListener("click", () => {
+            toggleTaskDone(index);
+         });
+      });
+   };
 
    const onFormSubmit = (event) => {
       event.preventDefault();
